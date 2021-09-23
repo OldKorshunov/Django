b45bdc61c0846e73cce0f89643e7bbfe8012b18a
OldKorshunov <dmkorshunov2013@gmail.com>
2021-09-24 01:55:09 +0300

    First and second homework with practice

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path
+
+from .views import index, main_article, archive_article, users, hexadecimal, random_slug, random_page
+from .views import article_number, article_text, article_user_number, phone_number, first
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('first', first, name='first'),
+    path('articles', main_article, name='main_article'),
+    path('articles/archive/', archive_article, name='archive_article'),
+    path('users/', users, name='users'),
+    path('random_page/', random_page, name='random_page'),
+    path('random_slug/', random_slug, name='random_slug'),
+    path('article/<int:article_number>/', article_number, name='article_number'),
+    path('article/<int:article_number>/<slug:slug_text>', article_text, name='article_text'),
+    path('users/<int:user_number>/', article_user_number, name='article_user_number'),
+    re_path(r'^articles/(050|063|095)([0-9]{7})/', phone_number, name='phone_number'),
+    re_path(r'^articles/(([a-f]|[0-9]){4}-([a-f]|[0-9]){6})/', hexadecimal, name='hexadecimal')
+]
+
+#urlpatterns = [
+   
+#]
